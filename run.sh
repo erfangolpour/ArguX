75084ffe4777452e63a40714ce56e5a7d9fa3f66
Erfan Golpour <golpourerfan@gmail.com>
2025-04-26 06:02:59 -0700

    Add run.sh to start services from single script

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 tmux new-session -d -s mysession
 
-tmux split-window -h -t mysession 'cd backend && source .venv/bin/activate && uvicorn main:app --reload'
-tmux split-window -v -t mysession 'cd backend && source .venv/bin/activate && celery -A main.celery worker -l info --concurrency=3'
+tmux split-window -h -t mysession 'cd backend && source .venv/bin/activate && uvicorn main:app --reload --log-level debug'
+tmux split-window -v -t mysession 'cd backend && source .venv/bin/activate && celery -A main.celery_app worker --loglevel=info --concurrency=3'
 tmux split-window -v -t mysession 'cd frontend && npm run dev'
 
 tmux select-layout tiled
